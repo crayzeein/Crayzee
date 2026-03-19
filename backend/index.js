a27c6575d7607958089a422901a2f758f5191462
Harshit-Chaturvedi <harshit091102@gmail.com>
2026-03-19 12:00:16 +0530

    Update API URL to env variable, add CORS for production domains

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: ['https://crayzee.in', 'https://www.crayzee.in', 'http://localhost:3000', 'http://localhost:3001'],
+  credentials: true
+}));
 app.use(helmet());
 app.use(morgan('dev'));
 
